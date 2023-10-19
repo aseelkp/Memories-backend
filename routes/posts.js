3896b1dfd3cfebf22d556a5be2a91c77da370f06
aseelkp <aseelaroor10@gmail.com>
2023-10-19 20:23:44 +0530

    feat : add pagination

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createPost, getPosts , updatePost , deletePost , likePost} from "../controllers/posts.js";
+import { getPostsBySearch , createPost, getPosts , updatePost , deletePost , likePost} from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 
 
 const router = express.Router();
 
+router.get('/search' , getPostsBySearch)
 router.get('/' , getPosts)
 router.post('/' , auth , createPost)
 router.patch('/:id', auth , updatePost)
