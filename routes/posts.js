42f9d421aa3370834aeb50220d60b081003f1d2a
aseelkp <aseelaroor10@gmail.com>
2023-10-19 23:53:00 +0530

    add : route and controller for getPost

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPostsBySearch , createPost, getPosts , updatePost , deletePost , likePost} from "../controllers/posts.js";
+import { getPostsBySearch , createPost, getPosts , updatePost , deletePost , likePost , getPost} from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 
@@ -7,6 +7,7 @@ import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.get('/search' , getPostsBySearch)
+router.get('/:id', getPost)
 router.get('/' , getPosts)
 router.post('/' , auth , createPost)
 router.patch('/:id', auth , updatePost)
