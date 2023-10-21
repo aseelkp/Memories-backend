ddd9fedbdb02a573734d04ae898f65729e5c9ee0
aseelkp <aseelaroor10@gmail.com>
2023-10-21 23:42:53 +0530

    add comment controller , route and db schema

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPostsBySearch , createPost, getPosts , updatePost , deletePost , likePost , getPost} from "../controllers/posts.js";
+import { getPostsBySearch , createPost, getPosts , updatePost , deletePost , likePost , getPost , commentPost } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 
@@ -13,5 +13,6 @@ router.post('/' , auth , createPost)
 router.patch('/:id', auth , updatePost)
 router.delete('/:id' , auth, deletePost)
 router.patch('/:id/likePost', auth , likePost)
+router.post('/:id/commentPost', auth , commentPost)
 
 export default router;
